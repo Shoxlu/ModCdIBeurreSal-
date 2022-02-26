461acfea1155192b83fec5133a4edcbb98a5efd7
Shoxlu <shoxlu0712@gmail.com>
2022-02-26 12:11:47 +0100

    Initial commit

---
+++ th15.v1.00b.js
@@ -0,0 +1,58 @@
+{
+  "binhacks": {
+	"Global_var_chapter_retry_counter": {
+		"addr": "0x42ef0a",
+		"expected": "A1 F8734E00",
+		"code": "A1 B8754E00",
+		},
+	"Escaping_BossShow":{
+		"addr": "0x438780",
+		"code": "E9 75530800 90 90 90 90",
+		},
+	"Changing_loaded_slot_in_memory_for_Sanae": {
+		"addr": "0x4BDAEE",
+		"code": "C705 98444E00 03000000\
+// EB 14\
+// 803D 04744E00 02\
+// 75 19\
+// 90\
+// C705 98444E00 04000000\
+// 8B4CC2 50\
+// A1 809A4E00\
+// E9 6DACF7FF\
+// 803D 04744E00 00\
+// 75 C9\
+// EB DD\
+// ",
+		}
+	}
+}
+²²
+// "C705 98444E00 03000000\
+// EB 14\
+// 803D 04744E00 02\
+// 75 19\
+// 90\
+// C705 98444E00 04000000\
+// 8B4CC2 50\
+// A1 809A4E00\
+// E9 8DACF7FF\
+// 803D 04744E00 00\
+// 75 C9\
+// EB DD\
+// "
+1er:
+EAX : 0C43F84C
+
+ESI: 10AF02F0
+
+2ème: 
+EAX : 05F6F770
+
+
+
+ESI 0C4700CC
+
+
+
+
---
+++ th15/compile stage 6.bat
@@ -0,0 +1,5 @@
+thecl -c 15 -m th13map.txt st06bsalt.tecl st06bsalt.ecl
+thecl -c 15 -m th13map.txt st06bs.tecl st06bs.ecl
+thecl -c 15 -m th13map.txt st06_1.tecl st06_1.ecl
+thecl -c 15 -m th13map.txt st06_2.tecl st06_2.ecl
+thecl -c 15 -m th13map.txt st06.tecl st06.ecl
