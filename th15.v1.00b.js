6f6c74a59f36ba0cb94ffee41afa4ff66c34833a
Shoxlu <shoxlu0712@gmail.com>
2022-04-24 19:13:03 +0200

    update binhacks + face01an.png

--- th15.v1.00b.js
+++ th15.v1.00b.js
@@ -1,5 +1,5 @@
 {
-  "binhacks": {
+	"binhacks": {
 	"Global_var_chapter_retry_counter": {
 		"addr": "0x42ef0a",
 		"expected": "A1 F8734E00",
@@ -23,36 +23,26 @@
 // 803D 04744E00 00\
 // 75 C9\
 // EB DD\
-// ",
+//",
 		},
-		
-	}
+	"EscapingSnakesColor": {
+		"addr": "0x004095B0",
+		"code": "E9 5A490B00 <nop>"
+		},
+	"NewCodeSnakesColor": {
+		"addr": "0x4BDF0F",
+		"code": "C706 000070FF\
+83FA 08\
+0F8D A8B6F4FF\
+8ACA\
+0C FF\
+90\
+C0E1 05\
+2AC1\
+8846 01\
+E9 96B6F4FF\
+90\
+",
+		},
+    }
 }
-// "C705 98444E00 03000000\
-// EB 14\
-// 803D 04744E00 02\
-// 75 19\
-// 90\
-// C705 98444E00 04000000\
-// 8B4CC2 50\
-// A1 809A4E00\
-// E9 8DACF7FF\
-// 803D 04744E00 00\
-// 75 C9\
-// EB DD\
-// "
-1er:
-EAX : 0C43F84C
-
-ESI: 10AF02F0
-
-2ème: 
-EAX : 05F6F770
-
-
-
-ESI 0C4700CC
-
-
-
-
