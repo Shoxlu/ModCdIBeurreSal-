56df1183384694f5bda07ee7b42d39928d30bd74
Shoxlu <shoxlu0712@gmail.com>
2022-05-31 23:41:19 +0200

    Update th15.v1.00b.js

--- th15.v1.00b.js
+++ th15.v1.00b.js
@@ -1,47 +1,73 @@
 {
-	"binhacks": {
-	"Global_var_chapter_retry_counter": {
-		"addr": "0x42EC06",
-		"code": "A1 B8754E00 8BE5 5D C2 0400",
-		},
-	"Escaping_BossShow":{
-		"addr": "0x438780",
-		"code": "E9 75530800 90 90 90 90",
-		},
-	"Changing_loaded_slot_in_memory_for_Sanae": {
-		"addr": "0x4BDAEE",
-		"code": "C705 98444E00 03000000\
-// EB 14\
-// 803D 04744E00 02\
-// 75 19\
-// 90\
-// C705 98444E00 04000000\
-// 8B4CC2 50\
-// A1 809A4E00\
-// E9 6DACF7FF\
-// 803D 04744E00 00\
-// 75 C9\
-// EB DD\
-//",
-		},
-	"EscapingSnakesColor": {
-		"addr": "0x004095B0",
-		"code": "E9 5A490B00 <nop>"
-		},
-	"NewCodeSnakesColor": {
-		"addr": "0x4BDF0F",
-		"code": "C706 0000FFFF\
-83FA 08\
-0F8D A8B6F4FF\
-8ACA\
-0C 5F\
-90\
-C0E1 05\
-2AC1\
-8846 01\
-E9 96B6F4FF\
-90\
-",
-		},
+  "binhacks": {
+    "Global_var_chapter_retry_counter": {
+      "addr": 0x42EC06,
+      "code": "\
+A1 <0x4E75B8> \
+C9 \
+C2 0400 \
+<int3:22> \
+"
+    },
+    "Escaping_BossShow": {
+      "addr": 0x438780,
+      "code": "\
+E9 [codecave:Changing_loaded_slot_in_memory_for_Sanae] \
+<int3:4> \
+"
+    },
+    "EscapingSnakesColor": {
+      "addr": 0x4095AC,
+      "code": "\
+<nop:4> \
+E9 [codecave:NewCodeSnakesColor] \
+<int3:17> \
+"
     }
+  },
+  "codecaves": {
+    "Changing_loaded_slot_in_memory_for_Sanae": {
+      "access": "re",
+      "code": "\
+8B0D <0x4E7404> \
+83E1 FD \
+83F9 01 \
+B9 03000000 \
+83D1 00 \
+890D <0x4E4498> \
+8B4CC2 50 \
+A1 <0x4E9A80> \
+E9 [0x438789] \
+"
+    },
+    "NewCodeSnakesColor": {
+      "access": "re",
+      "code": "\
+C706 0000FFFF \
+0C 5F \
+C0E1 05 \
+80FA 08 \
+0F8D [0x4095C6] \
+8ACA \
+2AC1 \
+8846 01 \
+E9 [0x4095C6] \
+"
+    }
+  }
 }
+// "codecaves": {
+    // "Changing_loaded_slot_in_memory_for_Sanae": {
+      // "access": "re",
+      // "code": "803D 04744E00 02\
+74 16\
+803D 04744E00 00\
+74 0D\
+C705 98444E00 03000000\
+EB 0B\
+90\
+C705 98444E00 04000000\
+8B4CC2 50\
+A1 809A4E00\
+E9 [0x438789]",
+    // },
