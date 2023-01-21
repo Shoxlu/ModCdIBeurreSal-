b1b7e37c85efe372fceb4df8ac7b61ab2ba12661
1Lance <104199156+1Lance@users.noreply.github.com>
2023-01-21 17:00:17 +0100

    day_mbs_spell

--- th15/spells.js
+++ th15/spells.js
@@ -1,5 +1,6 @@
 {
 	"10": "Signe du gras «Éclat Beurré»",
+	"23": "Illumination « Balles légendaires »",
     "58": "Douleur «Brûlure Lunaire»",
     "60": "Désespoir «Duplicité de la Fille Lunatique»",
     "76": "« La théorie du complot d'Apollo »",
