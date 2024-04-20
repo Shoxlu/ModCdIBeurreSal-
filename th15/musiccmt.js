fff611d1cd2d87842cd445f5194ac28ca54b1cc3
Baptoast <93494690+Baptoast@users.noreply.github.com>
2024-04-20 12:13:04 -0400

    Add files via upload

--- th15/musiccmt.js
+++ th15/musiccmt.js
@@ -4,161 +4,54 @@
         "　",
         "Le thème du menu.",
         "",
-        "Ce thème s'est révélé un peu spatial et japonais.",
-        "Pour ce jeu, le facteur d'excitation peut être décrit comme « inhabituel »."
+        "J'ai voulus donner une ambiance celtique et japonais.",
+        "Je suis assez content du résultat :D."
     ],
     "2": [
         "@",
         "　",
         "Le thème du premier niveau.",
         "",
-        "Cette musique est un peu longue pour le premier niveau.",
-        "Le jeu est assez long en général, donc je l'ai conçue pour que ça corresponde.",
-        "On a l'impression qu'elles ont décidé de quitter leur jolie Terre verte depuis un moment."
+        "Pour ce thème je voulais donner une impression de légèreté que l'on retrouve dans les premiers stages habituellement.",
+        "Plus on ce rapproche du boss, plus l'instrument electronique prend de l'ampleur."
     ],
     "3": [
         "@",
         "　",
-        "Le thème de Seiran.",
+        "Le thème de Shox.",
         "",
-        "J'ai fait ressortir le sentiment de passer un bon moment dans un combat vraiment joyeux.",
-        "Les lapins n'ont pas l'air de se montrer sérieux naturellement.",
-        "Vous ne pouvez pas le voir, mais elle a une queue."
+        "Déesse de la technologie ou je ne me souviens plus très bien, un thème remplis d'instruments électronique était obligatoire.",
     ],
     "4": [
         "@",
         "　",
         "Le thème du second niveau.",
         "",
-        "Nous rendant mal à l'aise avec l'évolution de la situation, cette musique pourrait avoir sa place dans la seconde moitié du jeu.",
-        "Il semblerait que seuls quelques privilégiés peuvent reconnaître le rythme émis par la Lune."
+        "Nous somme en guerre. Y'a des tirs de partout et pas une seconde de repos.",
+        "Je voulais faire un thème plus traditionnel, on a là des instruments assez simple avec des moments mystérieux qui font leurs charmes."
     ],
     "5": [
         "@",
         "　",
-        "Le thème de Ringo.",
+        "Le thème de Mariphaine.",
         "",
-        "Contrairement à Seiran, c'est une lapine qui travaille plutôt en arrière-plan.",
-        "En plus d'être une adversaire puissante, cette musique décrit ses doutes à propos de la nécessité de se battre.",
-        "Bien sûr, tous les dangos ont été appréciés par le personnel de la télévision après."
+        "Une sorcière membre de la secte du beurre doux...",
+        "J'ai donc essayer de faire un thème un peu maléfique qui reprend les instruments utiliser dans un des thème de Marisa."
     ],
     "6": [
         "@",
         "　",
         "Le thème du troisième niveau.",
         "",
-        "Cela ressemble à l'espace, mais c'est en fait un monde imaginaire. Donc c'est « simplement juste un rêve », dans un sens.",
-        "Les fées et les fantômes qui se promènent par ici représentent les rêves de ces dites créatures dans la réalité.",
-        "",
-        "C'est assez malicieux de leur part de répandre des barrages de danmaku même en rêve."
+        "A la base, l'ambiance était plutôt proche du dernier stage de UM (touhou 18), mais on à changé celà.",
+        "Ici on est sur une ambiance qui transporte entre calme et tension, on ce prépare à partir dans l'espace semble t-il..."
     ],
     "7": [
         "@",
         "　",
-        "Le thème de Doremy Sweet.",
+        "Le thème de Day.",
         "",
-        "Il dégage un fort sentiment d'appréhension.",
-        "J'ai écrit ceci en ayant cette pensée « Je veux que tu sois prudent à partir de maintenant »,",
-        "« Je veux que tu aies un cauchemar », ect.",
-        "Même si vous avez un cauchemar, cela ira mieux si vous priez pour le changer. Priez Doremy."
-    ],
-    "8": [
-        "@",
-        "",
-        "Le thème du quatrième niveau.",
-        "",
-        "C'est la capitale Lunaire. Cependant, pour une raison particulière, ce n'est pas une ville fantôme.",
-        "C'est une musique dans une ambiance sereine, mais qui reflète la folie.",
-        "",
-        "Cette musique conviendrait mieux à l'épilogue d'un jeu RPG que d'un jeu de tir. Mais bon, ça reste un épilogue."
-    ],
-    "9": [
-        "@",
-        "　",
-        "Le thème de Kishin Sagume.",
-        "",
-        "Un thème pour une mystérieuse personne qui n'est ni une alliée, ni une ennemie, ni une déesse, ni une oni.",
-        "De plus, j'ai composée ce thème en lui rendant une ambiance plutôt mystérieuse.",
-        "",
-        "Actuellement, c'est aussi une sorte d'amanojaku. Cela ferait donc deux en tout. Qui y aurait pensé ?"
-    ],
-    "17": [
-        "@",
-        "　",
-        "Le thème des crédits.",
-        "",
-        "Il s'agit d'une autre version du thème de l'écran titre.",
-        "",
-        "Je lui ai donné une ambiance plus extraterrestre que précédemment."
-    ],
-    "16": [
-        "@",
-        "　",
-        "Le thème de la fin.",
-        "",
-        "Eh bien, c'est le thème habituel.",
-        "",
-        "Parce que j'ai l'habitude d'écrire des musiques qui sont toujours enjoués.",
-        "J'ai l'impression que les musiques de jeux vidéos romans représentent un véritable défi."
-    ],
-    "10": [
-        "@",
-        "　",
-        "Le thème du cinquième niveau.",
-        "",
-        "Quelques changements mineurs au niveau de l'ambiance rendent cette mélodie dynamique et romantique.",
-        "Il s'agit d'un niveau où l'on peut progresser (et récolter énormément de points) à un bon rythme.",
-        "C'est bien agréable.",
-        "J'espère que vous avez la sensation de vous envoler et la volonté de voyager loin."
-    ],
-    "11": [
-        "@",
-        "　",
-        "Le thème de Clownpiece.",
-        "",
-        "C'est la fée la plus folle et robuste que nous ayons vu. Même si elle est plutôt normale dans sa tête.",
-        "J'ai rendu cette musique à la fois gothique, japonaise et extraterrestre, ce qui est assez bizarre.",
-        "La peur des clowns s'est beaucoup répandue de nos jours.",
-        "Mais cette fée clown n'est pas si effrayante que ça... si ?"
-    ],
-    "12": [
-        "@",
-        "　",
-        "Le thème du sixième niveau.",
-        "",
-        "C'est la meeeer. C'est l'étééééé.",
-        "",
-        "Cette musique est un sous-ensemble du thème du boss final.",
-        "Je veux aller à la meeeer. Je veux boire de la bière dans une cabane de plaaaaage."
-    ],
-    "13": [
-        "@",
-        "　",
-        "Le thème de Junko.",
-        "",
-        "Je pense que cette musique dit « Aucun doute, c'est le boss final ! »",
-        "Je n'ai pas cherché à la rendre trop compliquée, à ajouter quelques éléments sympas, ou simplement à la réanalyser en général.",
-        "",
-        "J'espère que vous apprécierez son danmaku purement et simplement."
-    ],
-    "14": [
-        "@",
-        "　",
-        "Le thème du niveau extra.",
-        "",
-        "Tout d'abord, une intro étrange, puis qui se jette dans style Touhou simple et habituel.",
-        "Il donne vraiment cette sensation Extra.",
-        "Néanmoins, est-ce que Doremy est votre ennemie cette fois ?",
-        "Ou votre alliée ?"
-    ],
-    "15": [
-        "@",
-        "　",
-        "Le thème d'Hecatia Lapislazuli",
-        "",
-        "C'est un morceau de musique assez démoniaque.",
-        "Je pensais au départ en faire une chanson du style Metal, mais je ne me connais pas très bien dans ce ",
-        "domaine, et je me disais que ça n'irait pas avec le contexte, donc je l'ai faite comme ceci à la fin.",
-        "Mon processus de réflexion était Enfer -> Shinigami -> Mort -> Death Metal. Assez mauvaise idée."
+        "Thème très étrange, qui convient à un boss très étrange également.",
+        "On à un peu de tension avec des instants de relâche inattendu..."
     ]
 }
