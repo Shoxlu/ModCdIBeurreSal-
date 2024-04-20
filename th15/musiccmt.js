3dc2446a62ec0a25a3001cef5074ad39d72a9f9c
Baptoast <93494690+Baptoast@users.noreply.github.com>
2024-04-20 12:16:25 -0400

    Add files via upload

--- th15/musiccmt.js
+++ th15/musiccmt.js
@@ -12,23 +12,29 @@
         "　",
         "Le thème du premier niveau.",
         "",
-        "Pour ce thème je voulais donner une impression de légèreté que l'on retrouve dans les premiers stages habituellement.",
-        "Plus on ce rapproche du boss, plus l'instrument electronique prend de l'ampleur."
+        "Pour ce thème je voulais donner une impression de légèreté que",
+        "l'on retrouve dans les premiers stages habituellement.",
+        "Plus on ce rapproche du boss, plus l'instrument electronique",
+        "prend de l'ampleur."
     ],
     "3": [
         "@",
         "　",
         "Le thème de Shox.",
         "",
-        "Déesse de la technologie ou je ne me souviens plus très bien, un thème remplis d'instruments électronique était obligatoire.",
+        "Déesse de la technologie ou je ne me souviens plus très bien, un",
+        "thème remplis d'instruments électronique était obligatoire."
     ],
     "4": [
         "@",
         "　",
         "Le thème du second niveau.",
         "",
-        "Nous somme en guerre. Y'a des tirs de partout et pas une seconde de repos.",
-        "Je voulais faire un thème plus traditionnel, on a là des instruments assez simple avec des moments mystérieux qui font leurs charmes."
+        "Nous somme en guerre. Y'a des tirs de partout et pas une seconde",
+        "de repos.",
+        "Je voulais faire un thème plus traditionnel, on a là des ",
+        "instruments assez simple avec des moments mystérieux qui ",
+        "font leurs charmes."
     ],
     "5": [
         "@",
@@ -36,22 +42,25 @@
         "Le thème de Mariphaine.",
         "",
         "Une sorcière membre de la secte du beurre doux...",
-        "J'ai donc essayer de faire un thème un peu maléfique qui reprend les instruments utiliser dans un des thème de Marisa."
+        "J'ai donc essayer de faire un thème un peu maléfique qui reprend",
+        "les instruments utiliser dans un des thème de Marisa."
     ],
     "6": [
         "@",
         "　",
         "Le thème du troisième niveau.",
         "",
-        "A la base, l'ambiance était plutôt proche du dernier stage de UM (touhou 18), mais on à changé celà.",
-        "Ici on est sur une ambiance qui transporte entre calme et tension, on ce prépare à partir dans l'espace semble t-il..."
+        "A la base, l'ambiance était plutôt proche du dernier stage ",
+        "de UM (touhou 18), mais on à changé celà.",
+        "Ici on est sur une ambiance qui transporte entre calme et ",
+        "tension, on ce prépare à partir dans l'espace semble t-il..."
     ],
     "7": [
         "@",
         "　",
         "Le thème de Day.",
         "",
-        "Thème très étrange, qui convient à un boss très étrange également.",
+        "Thème très étrange, qui convient à un boss très étrange aussi.",
         "On à un peu de tension avec des instants de relâche inattendu..."
     ]
 }
