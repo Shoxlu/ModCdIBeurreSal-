dc170f0b14f648c815e686707b19472b166e5fb4
1Lance <104199156+1Lance@users.noreply.github.com>
2024-05-31 22:43:25 +0200

    Compile stage 4 .bat

---
+++ th15/compile stage 4.bat
@@ -0,0 +1,6 @@
+thecl -c 15 -m th13map.txt st04bs.tecl st04bs.ecl
+thecl -c 15 -m th13map.txt st04.tecl st04.ecl
+trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04a.msg
+trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04b.msg
+trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04c.msg
+trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04d.msg
