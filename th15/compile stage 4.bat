3c4bd9f12771680ed31150a8564e90635f73f951
1Lance <104199156+1Lance@users.noreply.github.com>
2024-06-07 22:55:29 +0200

    Dialogues stage 4

--- th15/compile stage 4.bat
+++ th15/compile stage 4.bat
@@ -1,5 +1,6 @@
 thecl -c 15 -m th13map.txt st04bs.tecl st04bs.ecl
 thecl -c 15 -m th13map.txt st04.tecl st04.ecl
+thecl -c 15 -m th13map.txt st04mbs.tecl st04mbs.ecl
 trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04a.msg
 trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04b.msg
 trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04c.msg
