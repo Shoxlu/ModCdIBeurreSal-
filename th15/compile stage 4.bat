b565d2c9190a4dcdbe1f780199cda50ae0d01931
1Lance <104199156+1Lance@users.noreply.github.com>
2024-06-16 19:18:35 +0200

    msg stuff

--- th15/compile stage 4.bat
+++ th15/compile stage 4.bat
@@ -2,6 +2,6 @@ thecl -c 15 -m th13map.txt st04bs.tecl st04bs.ecl
 thecl -c 15 -m th13map.txt st04.tecl st04.ecl
 thecl -c 15 -m th13map.txt st04mbs.tecl st04mbs.ecl
 trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04a.msg
-trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04b.msg
-trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04c.msg
-trumsg compile -g 15 -m map/any.msgm st04a.mmsg -o st04d.msg
+trumsg compile -g 15 -m map/any.msgm st04b.mmsg -o st04b.msg
+trumsg compile -g 15 -m map/any.msgm st04c.mmsg -o st04c.msg
+trumsg compile -g 15 -m map/any.msgm st04d.mmsg -o st04d.msg
