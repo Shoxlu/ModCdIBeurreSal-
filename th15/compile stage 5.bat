0ad6bd00bcda785fbcfb0b59b0b06b79c09a20f8
1Lance <104199156+1Lance@users.noreply.github.com>
2024-06-17 15:57:04 +0200

    fix une erreur de con dans mon .bat

--- th15/compile stage 5.bat
+++ th15/compile stage 5.bat
@@ -1,3 +1,3 @@
 thecl -c 15 -m th13map.txt st05bs.tecl st04bs.ecl
-thecl -c 15 -m th13map.txt st05.tecl st04.ecl
+thecl -c 15 -m th13map.txt st05.tecl st05.ecl
 thecl -c 15 -m th13map.txt st05mbs.tecl st05mbs.ecl
